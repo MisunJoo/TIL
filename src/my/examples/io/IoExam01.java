b139402c76bac397d94facffd05fe2cb45ac8e4c
MisunJoo <0987someday@naver.com>
2018-09-28 17:02:48 +0900

    IoExam01

---
+++ src/my/examples/io/IoExam01.java
@@ -0,0 +1,42 @@
+package my.examples.io;
+
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
+
+import java.io.*;
+
+//1 바이트씩 읽어서 1 바이트씩 쓰기
+public class IoExam01 {
+    public static void main(String[] args){
+        InputStream in = null;
+        OutputStream out = null;
+
+        try {
+            File file = new File("/Users/misun/Documents/Java/TIL/README.md");
+
+            if(!file.isDirectory() && file.exists()) {
+
+                in = new FileInputStream(file);
+                out = new FileOutputStream("/tmp/readme.out");
+
+                int readData = 0;
+                while ((readData = in.read()) != -1) {
+                    out.write(readData);
+                }
+            }
+
+        }catch (IOException ioe){
+            ioe.printStackTrace();
+        }finally {
+            try {
+                in.close();
+            }catch (IOException ioe){}
+            try{
+                out.close();
+            }catch (IOException ioe){}
+        }
+
+        int readCount = 0;
+
+
+    }
+}
